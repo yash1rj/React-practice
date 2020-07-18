500d1ce133bd1ce00099891ca4caa53d129d8893
Yash Raj <yashrj130@gmail.com>
2020-07-18 22:04:29 +0530

    practice to be continued

--- createreactapptest/src/App.js
+++ createreactapptest/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Person from "./Person/Person"
+
+class App extends Component {
+
+  state={
+    persons:[
+      {name: "Max", age: 26},
+      {name: "Manu", age: 25}
+    ]
+  };
+
+  switchNameHandler = () => {
+    console.log("Got clicked!");
+    // DON'T USE : this.state.persons[0].name = "Raj"
+    this.setState({
+      persons:[
+        {name: "Raj", age: 26},
+        {name: "Manu", age: 30}
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+        <button onClick={this.switchNameHandler}>Click</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobby is Travelling.</Person>
+        <Person name="Ram" age= "29" />
+      </div>
+    )
+    // return React.createElement('div', { className: 'App'}, React.createElement('h1', null, "Hello, React!"));
+  }
 }
 
 export default App;
--- createreactapptest/src/index.js
+++ createreactapptest/src/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(
@@ -11,17 +11,7 @@ import * as serviceWorker from './serviceWorker';
 //   document.getElementById('root')
 // );
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, React!</h1>
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
