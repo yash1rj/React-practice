fc754500c5a13b61a5e1437e4af734cde71084ed
Yash Raj <yashrj130@gmail.com>
2020-08-26 01:54:37 +0530

    Create CLock.js

---
+++ tickingclock/src/Clock/CLock.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const clock = function Clock(props) {
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {props.date.toLocaleTimeString()}.</h2>
+        </div>
+    );
+}
+
+export default clock;
