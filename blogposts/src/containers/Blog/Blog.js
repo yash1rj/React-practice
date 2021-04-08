18c35ea8ecfd9c35de38218d1d37737e481f5c47
Yash Raj <yashrj130@gmail.com>
2021-04-08 16:14:22 +0530

    Restructuring Blog component

--- blogposts/src/containers/Blog/Blog.js
+++ blogposts/src/containers/Blog/Blog.js
@@ -1,66 +1,47 @@
 import React, { Component } from 'react';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
+
+import { NavLink, Route, Switch } from 'react-router-dom';
+
 // import axios from 'axios';
-import axios from '../../axios';
+// import axios from '../../axios';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPost: null,
-        error: false
-    }
-
-    componentDidMount() {
-        axios.get('/posts')
-            .then((response) => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: "Yash"
-                    }
-                })
-                this.setState({ posts: updatedPosts })
-                //console.log(response);
-            })
-            .catch(error => {
-                //console.log(error);
-                this.setState({ error: true })
-            });
-    }
-
-    postSelectHandler = (id) => {
-        this.setState({ selectedPost: id });
-    }
 
     render() {
-        let posts = <p style={{ textAlign: 'center' }}>Something went wrong !!</p>
-        if (!this.state.error) {
-            posts = this.state.posts.map((post) => {
-                return <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    selected={() => this.postSelectHandler(post.id)} />
-            });
-        }
 
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPost} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink
+                                to="/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: "/new-post",
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                <Route path="/" exact component={Posts} />
+                <Switch>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
