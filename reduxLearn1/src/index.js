b081af21dd067c1ec03e2af7341e35964c47bba4
Yash Raj <yashrj130@gmail.com>
2021-05-02 14:44:22 +0530

    Create index.js
    Updating index.js file with:
    -importing redux
    -creating central store
    -connecting reducer to store

---
+++ reduxLearn1/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import {createStore} from 'redux';
+import reducer from '../src/store/reducer';
+
+const store = createStore(reducer);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
