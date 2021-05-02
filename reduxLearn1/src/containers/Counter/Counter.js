abe9885acbe7fb408728e708550ec0d9cdcb6ebb
Yash Raj <yashrj130@gmail.com>
2021-05-02 16:30:55 +0530

    Update Counter.js
    Dispatching actions

--- reduxLearn1/src/containers/Counter/Counter.js
+++ reduxLearn1/src/containers/Counter/Counter.js
@@ -1,41 +1,64 @@
 import React, { Component } from 'react';
+import {connect}  from 'react-redux';
+// connect is a fn which returns a HOC
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
+    // state = {
+    //     counter: 0
+    // }
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
+    // counterChangedHandler = ( action, value ) => {
+    //     switch ( action ) {
+    //         case 'inc':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
+    //             break;
+    //         case 'dec':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
+    //             break;
+    //         case 'add':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
+    //             break;
+    //         case 'sub':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
+    //             break;
+    //     }
+    // }
 
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAddFiveToCounter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractFiveToCounter}  />
             </div>
         );
     }
 }
 
-export default Counter;
+// we pass 2 info on connect
+// no.1 the state slice relevant to this container
+// no.2 which actions are needed to be dispatched by this container
+// if we dont have actions in our container we can leave it out
+// if we only need to dispatch actions and dont need state slice from store pass it as null
+
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAddFiveToCounter: () => dispatch({type: 'ADDFIVE', value: 5}),
+        onSubtractFiveToCounter: () => dispatch({type: 'SUBTRACTFIVE', value: 5}),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter); //we execute the result of connect fn ( which is a fn ) on container
