85817edc98992a9f2eff278ce30cdbf578febf20
Yash Raj <yashrj130@gmail.com>
2021-05-02 19:13:25 +0530

    Update index.js
    -combining multiple reducers

--- reduxLearn1/src/index.js
+++ reduxLearn1/src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import reducer from '../src/store/reducer';
+import counterReducer from '../src/store/reducers/counter';
+import resultsReducer from '../src/store/reducers/results';
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+    ctr: counterReducer,
+    res: resultsReducer
+})
+
+const store = createStore(rootReducer);
 
 // provider is a helper component which allows us to kind of inject our store to react components
 // wrap the App component with Provider
