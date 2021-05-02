cba3cda1558c63cbe203b68b14e89fbf43d666cb
Yash Raj <yashrj130@gmail.com>
2021-05-02 16:29:03 +0530

    Update reducer.js
    Setting up multiple reducers

--- reduxLearn1/src/store/reducer.js
+++ reduxLearn1/src/store/reducer.js
@@ -3,6 +3,30 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
+    if(action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        };
+    }
+
+    if(action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        };
+    }
+
+    if(action.type === 'ADDFIVE') {
+        return {
+            counter: state.counter + action.value
+        };
+    }
+
+    if(action.type === 'SUBTRACTFIVE') {
+        return {
+            counter: state.counter - action.value
+        };
+    }
+
     return state;
 };
 
