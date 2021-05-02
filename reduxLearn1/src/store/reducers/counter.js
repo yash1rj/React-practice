a1c901efcb312cca2a287b0476ae122ce75905e6
Yash Raj <yashrj130@gmail.com>
2021-05-02 19:13:05 +0530

    Splitting into multiple reducers

--- reduxLearn1/src/store/reducers/counter.js
+++ reduxLearn1/src/store/reducers/counter.js
@@ -1,8 +1,7 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 
 const initialState = {
-    counter: 0,
-    results: []
+    counter: 0
 };
 
 const reducer = (state = initialState, action) => {
@@ -25,23 +24,6 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 counter: state.counter - action.value
-            };
-        case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({ id: new Date(), value: state.counter })
-            };//push changes the orginal property, but concat is used to immutably adding a new item
-        case actionTypes.DELETE_RESULT:
-            //1 way to immutably update array
-            // const id =2;
-            // const newArray = [...state.results];
-            // newArray.splice(id, 1);
-
-            //2nd way
-            const updatedArray = state.results.filter(result => result.id !== action.elId);
-            return {
-                ...state,
-                results: updatedArray
             };
 
     }
---
+++ reduxLearn1/src/store/reducers/results.js
@@ -0,0 +1,33 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+    results: []
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        case actionTypes.STORE_RESULT:
+            return {
+                ...state,
+                results: state.results.concat({ id: new Date(), value: action.counterVal })
+            };//push changes the orginal property, but concat is used to immutably adding a new item
+        case actionTypes.DELETE_RESULT:
+            //1 way to immutably update array
+            // const id =2;
+            // const newArray = [...state.results];
+            // newArray.splice(id, 1);
+
+            //2nd way
+            const updatedArray = state.results.filter(result => result.id !== action.elId);
+            return {
+                ...state,
+                results: updatedArray
+            };
+
+    }
+
+    return state;
+};
+
+export default reducer;
