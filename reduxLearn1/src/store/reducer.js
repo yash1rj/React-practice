53bca76c893250c2f9d5b38448340a960dae0476
Yash Raj <yashrj130@gmail.com>
2021-05-02 16:36:04 +0530

    Update reducer.js
    Implementing switch case in reducer

--- reduxLearn1/src/store/reducer.js
+++ reduxLearn1/src/store/reducer.js
@@ -3,31 +3,25 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-    if(action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        };
-    }
-
-    if(action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1
-        };
-    }
-
-    if(action.type === 'ADDFIVE') {
-        return {
-            counter: state.counter + action.value
-        };
-    }
-
-    if(action.type === 'SUBTRACTFIVE') {
-        return {
-            counter: state.counter - action.value
-        };
-    }
-
-    return state;
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            };
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            };
+        case 'ADDFIVE':
+            return {
+                counter: state.counter + action.value
+            };
+        case 'SUBTRACTFIVE':
+            return {
+                counter: state.counter - action.value
+            };
+        }
+        return state;
 };
 
 export default reducer;
