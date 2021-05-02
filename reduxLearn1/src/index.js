682c6972f149360daf612ab5169ad189eb6c90a7
Yash Raj <yashrj130@gmail.com>
2021-05-02 16:28:28 +0530

    Connecting react to redux
    -use of Provider helper component

--- reduxLearn1/src/index.js
+++ reduxLearn1/src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import {createStore} from 'redux';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import reducer from '../src/store/reducer';
 
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// provider is a helper component which allows us to kind of inject our store to react components
+// wrap the App component with Provider
+// hook up provider component with store by setting up store property
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
