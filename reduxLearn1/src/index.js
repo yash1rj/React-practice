3d83c9f29437169f49989c3a7943553b65e5e895
Yash Raj <yashrj130@gmail.com>
2021-05-03 11:36:55 +0530

    Update index.js
    - Implementing logger middleware
    - Configuring enhancers for redux dev tools support

--- reduxLearn1/src/index.js
+++ reduxLearn1/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import counterReducer from '../src/store/reducers/counter';
 import resultsReducer from '../src/store/reducers/results';
@@ -12,9 +12,22 @@ import resultsReducer from '../src/store/reducers/results';
 const rootReducer = combineReducers({
     ctr: counterReducer,
     res: resultsReducer
-})
+});
 
-const store = createStore(rootReducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState());
+            return result;
+        }
+    }
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 // provider is a helper component which allows us to kind of inject our store to react components
 // wrap the App component with Provider
