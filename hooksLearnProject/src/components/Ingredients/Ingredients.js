30d900cfacfe2fed28052c082aac9f990fc6c58c
Yash Raj <yashrj130@gmail.com>
2021-05-20 21:45:04 +0530

    Update Ingredients.js
    -connecting with firebase for adding new ingredients to db

--- hooksLearnProject/src/components/Ingredients/Ingredients.js
+++ hooksLearnProject/src/components/Ingredients/Ingredients.js
@@ -8,10 +8,18 @@ const Ingredients = () => {
   const [useringredients, setUserIngredients] = useState([]);
 
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevUserIngredients => [
-      ...prevUserIngredients,
-      { id: Math.random().toString(), ...ingredient }
-    ]);
+    fetch('https://react-hooks-basketapp-default-rtdb.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients(prevUserIngredients => [
+        ...prevUserIngredients,
+        { id: responseData.name, ...ingredient }
+      ]);
+    });
   };
 
   const removeIngredientHandler = id => {
