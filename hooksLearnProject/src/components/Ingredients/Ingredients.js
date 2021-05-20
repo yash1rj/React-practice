b82132918ba5539036dcc3b4eb3e34b5609ab175
Yash Raj <yashrj130@gmail.com>
2021-05-20 20:30:05 +0530

    Update Ingredients.js
    -add and remove ingredients handler implemented
    -ingredients state managed by Ingredients component

--- hooksLearnProject/src/components/Ingredients/Ingredients.js
+++ hooksLearnProject/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [useringredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    setUserIngredients(prevUserIngredients => [
+      ...prevUserIngredients,
+      { id: Math.random().toString(), ...ingredient }
+    ]);
+  };
+
+  const removeIngredientHandler = id => {
+    setUserIngredients(prevUserIngredients => {
+      return prevUserIngredients.filter(ing => {
+        return ing.id !== id
+      });
+    });
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredients={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={useringredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
