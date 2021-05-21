78e070831913c6916a8e6e05d2f21ea92ad9778a
Yash Raj <yashrj130@gmail.com>
2021-05-21 14:17:32 +0530

    Update Ingredients.js
    -converting useState usage to useReducer
    -useReducer helped in managing connected states easily

--- hooksLearnProject/src/components/Ingredients/Ingredients.js
+++ hooksLearnProject/src/components/Ingredients/Ingredients.js
@@ -1,14 +1,44 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useReducer, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
 import ErrorModal from '../UI/ErrorModal';
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch(action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id);
+    default:
+      throw new Error('Should not reach here!');
+  }
+};
+
+const httpReducer = (currentHttpState, action) => {
+  switch(action.type) {
+    case 'SEND':
+      return { loading: true, error: null };
+    case 'RESPONSE':
+      return { ...currentHttpState, loading: false };
+    case 'ERROR':
+      return { loading: false, error: action.errData };
+    case 'CLEAR_ERROR':
+      return { ...currentHttpState, error: null };
+    default:
+      throw new Error('Should not reach here!');
+  }
+};
+
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  const [httpState, dispatchHttp] = useReducer(httpReducer, { loading: false, error: null});
+  // const [userIngredients, setUserIngredients] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState();
 
   // we are already loading ingredients in our Search component
   // useEffect(() => {
@@ -33,52 +63,83 @@ const Ingredients = () => {
   }, [userIngredients]);
 
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
-    setUserIngredients(filteredIngredients);
+    // setUserIngredients(filteredIngredients);
+    dispatch({
+      type: 'SET',
+      ingredients: filteredIngredients
+    });
   }, []);
 
   const addIngredientHandler = ingredient => {
-    setIsLoading(true);
+    // setIsLoading(true);
+    dispatchHttp({
+      type: 'SEND'
+    });
     fetch('https://react-hooks-basketapp-default-rtdb.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
     }).then(response => {
-      setIsLoading(false);
+      // setIsLoading(false);
+      dispatchHttp({
+        type: 'RESPONSE'
+      });
       return response.json();
     }).then(responseData => {
-      setUserIngredients(prevUserIngredients => [
-        ...prevUserIngredients,
-        { id: responseData.name, ...ingredient }
-      ]);
+      // setUserIngredients(prevUserIngredients => [
+      //   ...prevUserIngredients,
+      //   { id: responseData.name, ...ingredient }
+      // ]);
+      dispatch({
+        type: 'ADD',
+        ingredient: { id: responseData.name, ...ingredient }
+      });
     });
   };
 
   const removeIngredientHandler = id => {
-    setIsLoading(true);
+    // setIsLoading(true);
+    dispatchHttp({
+      type: 'SEND'
+    });
     fetch(`https://react-hooks-basketapp-default-rtdb.firebaseio.com/ingredients/${id}.json`, {
       method: 'DELETE'
     }).then(response => {
-      setIsLoading(false);
-      setUserIngredients(prevUserIngredients => {
-        return prevUserIngredients.filter(ing => {
-          return ing.id !== id
-        });
+      // setIsLoading(false);
+      dispatchHttp({
+        type: 'RESPONSE'
+      });
+      // setUserIngredients(prevUserIngredients => {
+      //   return prevUserIngredients.filter(ing => {
+      //     return ing.id !== id
+      //   });
+      // });
+      dispatch({
+        type: 'DELETE',
+        id: id
       });
     }).catch(err => {
-      setError("Something went wrong!!");
-      setIsLoading(false);
+      // setError("Something went wrong!!");
+      // setIsLoading(false);
+      dispatchHttp({
+        type: 'ERROR',
+        errData: "Something went wrong!!"
+      });
     });
   };
 
   const clearError = () => {
-    setError(null);
+    // setError(null);
+    dispatchHttp({
+      type: 'CLEAR_ERROR'
+    });
   };
 
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
 
-      <IngredientForm onAddIngredients={addIngredientHandler} loading={isLoading} />
+      <IngredientForm onAddIngredients={addIngredientHandler} loading={httpState.loading} />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
