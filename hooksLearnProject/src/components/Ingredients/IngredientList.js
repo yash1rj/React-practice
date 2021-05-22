96eb554cba1d28ee0a0a362f21d74f8cd8bd59ba
Yash Raj <yashrj130@gmail.com>
2021-05-22 11:44:10 +0530

    removing console logs

--- hooksLearnProject/src/components/Ingredients/IngredientList.js
+++ hooksLearnProject/src/components/Ingredients/IngredientList.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './IngredientList.css';
 
 const IngredientList = props => {
-  console.log("RENDERING ING LIST");
+  // console.log("RENDERING ING LIST");
   return (
     <section className="ingredient-list">
       <h2>Loaded Ingredients</h2>
