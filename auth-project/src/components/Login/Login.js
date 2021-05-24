e2f2d6bc4b361b0e55c14d60bea582dbcebdc3e6
Yash Raj <yashrj130@gmail.com>
2021-05-25 00:06:03 +0530

    useImperativeHandle hook and forwardRef
    -making custom component binded with ref

--- auth-project/src/components/Login/Login.js
+++ auth-project/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useReducer, useState } from 'react';
+import React, { useContext, useEffect, useReducer, useRef, useState } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -40,6 +40,9 @@ const Login = (props) => {
   });
 
   const authCtx = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   const { isValid: emailIsValid } = emailState; // pulling out isValid from the obj and giving alias of emailIsValid
   const { isValid: passwordIsValid } = passwordState;
@@ -86,13 +89,22 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if(formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    }
+    else if(!emailIsValid) {
+      emailInputRef.current.focus();
+    }
+    else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id="email"
           label="E-Mail"
           isValid={emailIsValid}
@@ -102,6 +114,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler} />
 
         <Input
+          ref={passwordInputRef}
           id="password"
           label="Password"
           isValid={passwordIsValid}
@@ -111,7 +124,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler} />
 
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
