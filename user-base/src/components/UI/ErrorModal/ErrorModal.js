6731b7fd7fed8538a68d855c444fd1cb488ba5cd
Yash Raj <yashrj130@gmail.com>
2021-05-24 14:38:45 +0530

    using ReactDOM feature of createPortal to render modal and backdrop to root DOM

--- user-base/src/components/UI/ErrorModal/ErrorModal.js
+++ user-base/src/components/UI/ErrorModal/ErrorModal.js
@@ -3,23 +3,44 @@ import Button from '../Button/Button';
 import Card from '../Card/Card';
 import classes from './ErrorModal.module.css';
 
+import ReactDOM from 'react-dom';
+
+const Backdrop = props => {
+    return <div className={classes.backdrop} onClick={props.onErrorHandler} />;
+};
+
+const ModalOverlay = props => {
+    return (
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={classes.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={classes.actions}>
+                <Button onClick={props.onErrorHandler}>OK</Button>
+            </footer>
+        </Card>
+    );
+};
+
 const ErrorModal = props => {
 
     return (
-        <div>
-            <div className={classes.backdrop} onClick={props.onErrorHandler} />
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onErrorHandler}>OK</Button>
-                </footer>
-            </Card>
-        </div>
+        <React.Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onErrorHandler={props.onErrorHandler} />, 
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay 
+                    title={props.title}
+                    message={props.message}
+                    onErrorHandler={props.onErrorHandler} />, 
+                document.getElementById('overlay-root')
+            )}
+        </React.Fragment>
     );
 };
 
