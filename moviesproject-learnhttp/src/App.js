361e2297a66ef0d2975a849ff93c1173cc5eb567
Yash Raj <yashrj130@gmail.com>
2021-05-26 14:26:10 +0530

    Update App.js
    -fetching movies with HTTP GET

--- moviesproject-learnhttp/src/App.js
+++ moviesproject-learnhttp/src/App.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  
+  const [moviesList, setMoviesList] = useState([]);
+
+  const fetchMoviesHandler = () => {
+    fetch('https://swapi.dev/api/films')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.results);
+        const transformedMovies = data.results.map(movie => {
+          return {
+            id: movie.episode_id,
+            title: movie.title,
+            release: movie.release_date,
+            openingText: movie.opening_crawl
+          };
+        });
+        setMoviesList(transformedMovies);
+      });
+  };
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={moviesList} />
       </section>
     </React.Fragment>
   );
