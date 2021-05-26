2d1d160d3733e742011f9bc539948fa3603b6d0e
Yash Raj <yashrj130@gmail.com>
2021-05-26 17:53:54 +0530

    Restructuring to accomodate Tasks section

--- counterCustomHook/src/App.js
+++ counterCustomHook/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+import BackwardCounter from './counters-components/BackwardCounter';
+import ForwardCounter from './counters-components/ForwardCounter';
+import TaskApp from './tasks-components/TaskApp';
 
 function App() {
   return (
     <React.Fragment>
       <ForwardCounter />
       <BackwardCounter />
+      <TaskApp />
     </React.Fragment>
   );
 }
