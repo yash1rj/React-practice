196a83d235d319d63b8679a83028df87beb346b7
Yash Raj <yashrj130@gmail.com>
2021-05-26 23:46:37 +0530

    Update TaskItem.js
    -commented logic for deleting item

--- counterCustomHook/src/tasks-components/Tasks/TaskItem.js
+++ counterCustomHook/src/tasks-components/Tasks/TaskItem.js
@@ -1,6 +1,18 @@
+import React from 'react';
 import classes from './TaskItem.module.css';
 
 const TaskItem = (props) => {
+
+  // const { sendHttpRequest } = useHttp();
+
+  // const onDeleteHandler = (id) => {
+  //   props.onRemoveTask(id);
+  //   sendHttpRequest({
+  //     url: 'https://react-hooks-basketapp-default-rtdb.firebaseio.com/tasks/' + id + '.json/',
+  //     method: 'DELETE'
+  //   });
+  // };
+
   return <li className={classes.task}>{props.children}</li>
 };
 
